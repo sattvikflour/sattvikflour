6fc2da0491455454956935543655fc8a0ca17de4
Abhijit_Abdagire <admin@sattvikflour.com>
2024-02-14 23:48:47 +0530

    14 feb

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $this->call(UsersTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
         $this->call(ProductSeeder::class);
 
     }
