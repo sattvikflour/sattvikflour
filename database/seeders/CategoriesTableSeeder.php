6e40c4afeefb2678fe4f3cc1e097384ed5deeca6
Abhijit_Abdagire <admin@sattvikflour.com>
2024-02-15 00:01:52 +0530

    14 feb changes

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -17,35 +17,35 @@ class CategoriesTableSeeder extends Seeder
             [
                 "category_name" => "Wheat",
                 "category_url" => "wheat",
-                "category_img" => "20231226003856000.jpg",
+                "category_img" => "wheat.jpg",
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 "category_name" => "Jawar",
                 "category_url" => "jawar",
-                "category_img" => "20231226003856000.jpg",
+                "category_img" => "jawar.jpg",
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
-                "category_name" => "Bazari",
-                "category_url" => "bazari",
-                "category_img" => "20231226003856000.jpg",
+                "category_name" => "Bajari",
+                "category_url" => "bajari",
+                "category_img" => "bajari.jpg",
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 "category_name" => "Besan",
                 "category_url" => "besan",
-                "category_img" => "20231226003856000.jpg",
+                "category_img" => "besan.jpg",
                 'created_at' => now(),
                 'updated_at' => now()
             ],
             [
                 "category_name" => "Barley",
                 "category_url" => "barley",
-                "category_img" => "20231226003856000.jpg",
+                "category_img" => "barley.jpg",
                 'created_at' => now(),
                 'updated_at' => now()
             ]
